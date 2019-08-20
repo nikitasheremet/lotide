1205ca246624e49290c36f17b4b2dd9fe27eb3ce
nikitasheremet <nikita.sheremet253@gmail.com>
2019-08-20 19:04:36 +0000

    updated assertEqualArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,23 @@
-const assertArraysEqual = (array1, array2) => {
+const eqArrays = (array1, array2) => {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
-        console.log(`❌❌ ${array1} !== ${array2}`);
-        return;
+        return false;
       }
     }
-    console.log(`✅✅ ${array1} === ${array2}`);
   } else {
-    console.log(`❌❌ ${array1} !== ${array2}`);
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  if (actual === expected){
+    console.log(`✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌ ${actual} !== ${expected}`);
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(eqArrays([1,2,3],[1,2,3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
