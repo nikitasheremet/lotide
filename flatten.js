bbb0f40b8d7187b27c910a3e95d4c3656111c625
nikitasheremet <nikita.sheremet253@gmail.com>
2019-08-21 16:49:57 +0000

    created flatten.js

--- flatten.js
+++ flatten.js
@@ -25,4 +25,4 @@ const flatten = (array) => {
 
 // Test
 
-assertArraysEqual(flatten([1, 2, [3, 4,5,5], 5, [6]]),[1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]);
