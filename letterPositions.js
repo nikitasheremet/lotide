918949bdf2410361f630abda80eb95692222a09e
nikitasheremet <nikita.sheremet253@gmail.com>
2019-08-21 18:24:46 +0000

    made letterPosition

--- letterPositions.js
+++ letterPositions.js
@@ -1,12 +1,27 @@
+const eqArrays = (array1, array2) => {
+  output = true;
+  array1.length === array2.length || (output = false)
+  array1.forEach(element => {if (array2[array1.indexOf(element)] !== element) output =  false});
+  return output;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  console.log(eqArrays(actual,expected) ? `✅✅  ${actual} === ${expected}` : `❌❌  ${actual} !== ${expected}`);
+};
+
 const letterPosition = (string) => {
   result = {};
   let uniqueSet = new Set (string.split(" ").join(""));
-  uniqueSet.forEach(item => {
-    let regex = new RegExp (item, "g");
-    result[item] = string.match(regex);
-  });
-  console.log(result);
+  uniqueSet.forEach(item => result[item] = occurences(item,string));
   return result;
 };
 
-letterPosition("lighthouse in the house");
+function occurences(letter, string) {
+  output = [];
+  for (i in string) {
+    if (letter === string[i]) output.push(Number(i))
+  }
+  return output;
+}
+
+assertArraysEqual(letterPosition("hello").e,[1]);
