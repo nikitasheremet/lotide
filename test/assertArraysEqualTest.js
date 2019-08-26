02ce18777f46e9d62ea84dddb364cb7c1088a84d
nikitasheremet <nikita.sheremet253@gmail.com>
2019-08-26 13:54:10 +0000

    created middletest

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,5 @@
 const assertArraysEqual = require("../assertArraysEqual");
 const eqArrays = require("../eqArrays");
 
-assertArraysEqual(eqArrays([1,2,3],[1,2,3]), true);
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
