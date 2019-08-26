0200713bccff2d9e868dceed025c12027a00d4ed
nikitasheremet <nikita.sheremet253@gmail.com>
2019-08-26 14:11:35 +0000

    added mocha test for head

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,28 @@
 const assertEqual = require("../assertEqual.js");
 const head = require("../head");
+const chai = require("chai")
+const assert = chai.assert
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", function() {
+
+  it ("returns first value when passed array of numbers", function() {
+    let arr = [5,6,7];
+    result = head(arr);
+    assert.strictEqual(result,5);
+  })
+  it ("returns first value when passed array of strings", function() {
+    let arr = ["Hello", "Lighthouse", "Labs"];
+    result = head(arr);
+    assert.strictEqual(result,"Hello");
+  })
+  it ("returns undefined when passed empty array", function() {
+    let arr = [];
+    result = head(arr);
+    assert.strictEqual(result, undefined);
+  })
+  it("returns '5' for ['5']", function() {
+    let arr = ['5']
+    result = head(arr);
+    assert.strictEqual(result, '5'); 
+  });
+});
