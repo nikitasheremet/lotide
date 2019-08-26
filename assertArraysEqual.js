4f318edf389771ea587b763d69da6d9a0b7054eb
nikitasheremet <nikita.sheremet253@gmail.com>
2019-08-26 13:49:14 +0000

    fixed bug in assertArrayEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,7 @@
-const eqArrays = (array1, array2) => {
-  output = true;
-  array1.length === array2.length || (output = false)
-  array1.forEach(element => {if (array2[array1.indexOf(element)] !== element) output =  false});
-  return output;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = (actual, expected) => {
-  console.log(eqArrays(actual,expected) ? `✅✅  ${actual} === ${expected}` : `❌❌  ${actual} !== ${expected}`);
+  console.log((actual === expected) ? `✅✅  ${actual} === ${expected}` : `❌❌  ${actual} !== ${expected}`);
 };
 
-assertArraysEqual(eqArrays([1,2,3],[1,2,3]), true);
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+module.exports = assertArraysEqual;
--- eqArrays.js
+++ eqArrays.js
@@ -3,15 +3,14 @@ const assertEqual = function(actual, expected) {
 };
 const eqArrays = (array1, array2) => {
   output = true;
-  array1.length === array2.length || (output = false)
-  array1.
-    forEach((element,index) => {
-      if (Array.isArray(element)) {
-        output = eqArrays(element, array2[index]);
-      } else {
-        if (array2[index] !== element) output =  false
-      }
-    });
+  if (array1.length !== array2.length) return false;
+  array1.forEach((element,index) => {
+    if (Array.isArray(element)) {
+      output = eqArrays(element, array2[index]);
+    } else {
+      if (array2[index] !== element) output =  false
+    }
+  });
   return output;
 };
 
