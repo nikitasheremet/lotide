308afe1ba1237224da2da6a7244273c5dfcb44a9
nikitasheremet <nikita.sheremet253@gmail.com>
2019-08-26 13:54:40 +0000

    reverted previous fix as test was set up wrong and there was no bug

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,7 @@
 const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = (actual, expected) => {
-  console.log((actual === expected) ? `✅✅  ${actual} === ${expected}` : `❌❌  ${actual} !== ${expected}`);
+  console.log(eqArrays(actual,expected) ? `✅✅  ${actual} === ${expected}` : `❌❌  ${actual} !== ${expected}`);
 };
 
 module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,14 +1,3 @@
-const eqArrays = (array1, array2) => {
-  output = true;
-  array1.length === array2.length || (output = false)
-  array1.forEach(element => {if (array2[array1.indexOf(element)] !== element) output =  false});
-  return output;
-};
-
-const assertArraysEqual = (actual, expected) => {
-  console.log(eqArrays(actual,expected) ? `✅✅  ${actual} === ${expected}` : `❌❌  ${actual} !== ${expected}`);
-};
-
 const middle = (array) => {
   let pos = Math.floor((array.length / 2));
   if (array.length <= 2) {
@@ -20,9 +9,5 @@ const middle = (array) => {
   }
 };
 
-//Test
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
+module.exports = middle;
 
